0575ec9d4037a4a67770535034a21a134f13c5b0
gitSeong <gitseong@gmail.com>
2018-08-17 23:57:35 +0900

    redis 연동

--- modules/socket_module.js
+++ modules/socket_module.js
@@ -1,7 +1,25 @@
 module.exports = (server)=>{
 
     let io = require('socket.io')(server);
+    let redis = require('redis');
+    let client = redis.createClient();
 
+    client.on('error', (err)=>{
+        console.log(err);
+    });
+
+
+    client.set("testKey", "testValue", redis.print);
+    client.hmset('frameworks', {
+        'javascript': 'AngularJS',
+        'css': 'Bootstrap',
+        'node': 'Express'
+    });
+
+    client.hgetall('frameworks', (error, reply)=>{
+        console.log(error);
+        console.log(reply);
+    });
 
     // 필요한 이벤트
     /*
@@ -33,18 +51,39 @@ module.exports = (server)=>{
             }
 
             socket.join(data.room);
-          
+            io.to(data.room).emit("chat", "누군가 입장했어");
+            console.log(socket.rooms[Object.keys(socket.rooms)[0]]);
+
         });
 
         //사용자별 위치 입력 이벤트
         socket.on('MAP_PICK', (data)=>{
 
+            let lat = data.lat;
+            let long = data.long;
+            
+            let point = {
+                lat : lat,
+                long : long
+            };
+
+            io.to(data.room).emit("MAP_PICK", point);
+            
+            
         });
 
-        //위치 추천 정보 발생 이벤트.
-        socket.on('RECOMMAND', (data)=>{
+        //완료 버튼 눌렀을때
+        socket.on('COMPLETE', (data)=>{
+            // 중점찾기 알고리즘돌리고
+            
+            let point = {
+                lat : "위도",
+                long : "경도"
+            };
 
+            io.to(data.room).emit('COMPLETE', point );
         });
+
 
         //추천 리스트에서 선택 이벤트
         socket.on('LIST_PICK', (data)=>{
@@ -57,8 +96,8 @@ module.exports = (server)=>{
             let room = socket.room = data.room;
             
             socket.join(room);
-    
             io.emit('chat', data.msg);
+
         })
     });
 }
