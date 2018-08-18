54d8ae9aee06e98d8e5ca18daad501ab1a1d623c
gitSeong <gitseong@gmail.com>
2018-08-18 20:56:53 +0900

    이벤트 인터페이스 코드

--- modules/socket_module.js
+++ modules/socket_module.js
@@ -9,27 +9,30 @@ module.exports = (server)=>{
     });
 
 
-    client.set("testKey", "testValue", redis.print);
-    client.hmset('frameworks', {
-        'javascript': 'AngularJS',
-        'css': 'Bootstrap',
-        'node': 'Express'
-    });
+    // client.set("testKey", "testValue", redis.print);
+    // client.hmset('frameworks', {
+    //     'javascript': 'AngularJS',
+    //     'css': 'Bootstrap',
+    //     'node': 'Express'
+    // });
 
-    client.hgetall('frameworks', (error, reply)=>{
-        console.log(error);
-        console.log(reply);
-    });
+    // client.hgetall('frameworks', (error, reply)=>{
+    //     console.log(error);
+    //     console.log(reply);
+    // });
 
     // 필요한 이벤트
     /*
         
-        1. 방정보 입력 이벤트
-        2. 사용자별 위치 입력 이벤트
-        3. 위치 추천 정보 발생 이벤트.
-        4. 추천 리스트에서 선택 이벤트.
+        1. CONNECTION 
+        2. CREATE_ROOM
+        3. PICK
+        4. COMPLETE
+        5. RELOAD_ROOM
 
     */
+
+    //1. CONNECTION
     io.on('connection',(socket)=>{
 
         console.log('[SERVER] => Connection Establish....');
@@ -38,8 +41,41 @@ module.exports = (server)=>{
             msg : "SUCCESS"
         };
 
-        socket.emit('connection', connectionMsg);
+        socket.emit('CONNECTION', connectionMsg);
 
+        //2. CREATE_ROOM
+        socket.on('CREATE_ROOM', (data)=>{
+
+        });
+        
+        //3. PICK
+        socket.on('PICK', (data)=>{
+            let return_date = {
+                status : 200,
+                room : data.room,
+                lat : data.lat,
+                long : data.long
+            };
+
+            io.to(data.room).emit('PICK',return_data);
+
+        });
+        
+        //4. COMPLETE
+        socket.on('COMPLETE', (data)=>{
+            // 레디스에서 room이름의 데이터를 가져와서
+            // 위도 경도 정보를 뽑아내서
+            // 중점 좌료를 계산
+            // API를 호출하여 추천 위치리스트를 받음.
+            // 사용자들에게 뿌려준다.
+            
+        });
+        
+        //5. RELOAD_ROOM
+        socket.on('RELOAD_ROOM', (data)=>{
+
+        });
+        
         // 방 이름 입력했을때 이벤트
         socket.on('APP_GROUP', (data)=>{
             
