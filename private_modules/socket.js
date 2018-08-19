ecc9189dc77d98f5bca245124720ed0aae7f8866
gitSeong <gitseong@gmail.com>
2018-08-19 10:52:07 +0900

    방 정보 찾기 코드 수정

--- private_modules/socket.js
+++ private_modules/socket.js
@@ -179,7 +179,7 @@ module.exports = (server)=>{
         socket.on('ROOM_LIST', (data)=>{
             
             let list = {};
-
+           
             // 방정보들을 가져온다.
             data.room_codes.forEach(code => {
                 findRoom(room, function(reply){
@@ -187,8 +187,13 @@ module.exports = (server)=>{
                 });
             });
 
+            let result = {
+                "status" : 1,
+                "room_list" : list,
+                "msg" : "방 정보 찾기 성공"
+            }
             // 방정보 리스트 이벤트 발생.
-            socket.emit('ROOM_LIST', list);
+            socket.emit('ROOM_LIST', result);
         });
     });
 
