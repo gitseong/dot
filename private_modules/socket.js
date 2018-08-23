ee77c645d5fcaafeb462d41d8724c01823b94b8d
gitSeong <gitseong@gmail.com>
2018-08-23 23:41:48 +0900

    방 생성시 에러 수정

--- private_modules/socket.js
+++ private_modules/socket.js
@@ -69,13 +69,8 @@ module.exports = (server)=>{
             // 중복될 확률
             // 1/(26+26+9)^10 => 1/2^60;
             let room_code = randomstring.generate(10);
-
-            let params = [
-                room_code,
-                ,"room_name", data.room_name
-                ,"room_status", "진행중",
-            ];
-        
+            let params = [room_code, "room_name", data.room_name, "room_status" , "진행중"];
+            
             client.hmset(params, (error, reply)=>{
             
                 let result = {};
@@ -152,25 +147,43 @@ module.exports = (server)=>{
         
         //4. 입력 완료 이벤트
         socket.on('COMPLETE', (data)=>{
+           
             // 레디스에서 room이름의 데이터를 가져와서
             // 위도 경도 정보를 뽑아내서
             // 중점 좌료를 계산
             // API를 호출하여 추천 위치리스트를 받음.
             // 사용자들에게 뿌려준다.
 
-            let pos = {
-                "lat" : 37.4979462,
-                "long" : 127.025427
-            };
-            
-            let result = {
-                "status" : 1,
-                "room_code" : data.room_code,
-                "pick_list" : pos,
-                "msg" : "입력 완료 이벤트 성공"
-            }
+  
 
-            io.to(data.room_code).emit('COMPLETE', result);
+            client.hmset([data.room_code, "room_status" , "완료"], (error, reply)=>{
+                
+                let result = {}, pos = {};
+
+                try{
+                    if(error)
+                        throw new Error("방 상태정보 갱신 실패");
+
+                    pos = {
+                        "lat" : 37.4979462,
+                        "long" : 127.025427
+                    };
+                    
+                    result = {
+                        "status" : 1,
+                        "room_code" : data.room_code,
+                        "pick_list" : pos,
+                        "msg" : "입력 완료 이벤트 성공"
+                    }
+                }
+                catch(exception){
+                    result = errorInfo(0, "방 상태정보 갱신 실패");
+                    console.log(exception);
+                }
+                finally{
+                    io.to(data.room_code).emit('COMPLETE', result);
+                }
+            });
             
         });
         
